8260e79ac861cae0b66d0c48a9b26ab136d278da
Huynh Anh Trung <trungha@vnext.com.vn>
2020-08-20 10:59:15 +0700

    say hi methol for dog

---
+++ gitinit.js
@@ -0,0 +1,27 @@
+//mkdir Demo                    (tao moi folder)
+//cls                           (clear screen)
+
+// git init                     (init git vao project)
+// git status                   (trang thai hien tai)
+// git add                      (add component)
+    // git add .                (add tat ca)
+// git commit -m ""             (viet commit)
+
+// git log                      (show list commit)
+// git show (id)                (show commit theo id)
+//git diff                      (show diem khac nhau)
+
+//git checkout -- (component)   (xoa nhung thay doi o working directory)
+//git checkout -- (component) = git restore
+//git reset (component)         (xoa bo component da add)
+
+//git checkout -b (new branch)  (add branching)
+//git checkout branch Trung     (toi branch dang dung)
+//git merging branch master     (gop cac commit o branch Trung sang branch master)
+
+// git reset --soft(id)         (quay lai commit vs ma id)
+// git reset --hard(id)         (xoa -commit moi nhat- vs ma id)
+
+//git revert (id)               (xoa commit da~ cu~ vs ma id)
+
+//gitignore
